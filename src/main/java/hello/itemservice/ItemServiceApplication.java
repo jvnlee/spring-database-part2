11b2889eb57511fb57b53ee706446b25d9400b54
jvnlee <lhjun1028@naver.com>
2023-01-25 01:33:24 +0900

    feat: 테스트 환경에서 스프링이 제공하는 인메모리 H2 DB를 사용
    테스트 코드를 실행하기 위해 테스트용 DB를 따로 만들어두는 것은 번거로운 일이기에 테스트 용도의 일회용 DB를 사용하면 보다 간편해짐. 스프링 부트는 내부적으로 메모리를 공유하는 H2 DB를 제공함. 이 DB를 사용하기 위해 test profile을 지정하고 DataSource 빈을 직접 생성해서 등록함. DB를 생성할 때 테이블 정보가 필요하므로 schema.sql로 정의해두면 이를 바탕으로 초기화됨.

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 
 @Import(JdbcTemplateConfigV3.class)
@@ -21,6 +24,23 @@ public class ItemServiceApplication {
 	@Profile("local")
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
+	}
+
+	/*
+	test profile에 대해 (테스트 코드 수행 시) 적용
+	JVM 메모리를 공유하는 인메모리 H2 DB를 사용 (= 임베디드 모드)
+	JVM 부팅 시 생성되고, 종료시 같이 사라짐
+	 */
+	@Bean
+	@Profile("test")
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+
+		return dataSource;
 	}
 
 }
