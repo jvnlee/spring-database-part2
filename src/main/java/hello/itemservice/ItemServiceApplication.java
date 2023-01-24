9776bdc4495d0716637de7f5555211d7df1b4f6f
jvnlee <lhjun1028@naver.com>
2023-01-25 01:47:18 +0900

    refactor: test 디렉토리의 application.properties 내 datasource 정보 삭제 및 ItemServiceApplication의 test profile 빈 삭제
    테스트 디렉토리의 application.properties에 별도로 datasource 정보를 지정하지 않으면 스프링 부트는 인메모리 H2 DB를 사용하는 DataSource 빈을 자동적으로 생성해서 등록해줌. 따라서 이전 커밋에서처럼 인메모리 DB를 사용하기 위해 수기로 DataSource 빈 등록을 해줄 필요는 없고, schema 파일만 준비해두면 됨.

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -26,21 +26,4 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
-	/*
-	test profile에 대해 (테스트 코드 수행 시) 적용
-	JVM 메모리를 공유하는 인메모리 H2 DB를 사용 (= 임베디드 모드)
-	JVM 부팅 시 생성되고, 종료시 같이 사라짐
-	 */
-	@Bean
-	@Profile("test")
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.h2.Driver");
-		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-		dataSource.setUsername("sa");
-		dataSource.setPassword("");
-
-		return dataSource;
-	}
-
 }
