20cfe7c9acc12f988d43a68d58c85facf92e7c88
jvnlee <lhjun1028@naver.com>
2023-01-28 00:26:22 +0900

    feat: Item 도메인 클래스를 JPA Entity로 변경
    클래스 레벨에 @Entity 어노테이션을 붙여 JPA가 해당 클래스가 엔티티임을 인식시킴. id 프로퍼티에 @Id를 붙여 JPA가 PK로 인식하게 하고, @GeneratedValue로 해당 PK가 취할 전략을 지정함. (여기서는 DB가 직접 값을 생성하고 관리하는 전략) 그 외의 프로퍼티에는 선택적으로 @Column을 붙일 수 있는데, 프로퍼티와 컬럼의 이름이 아예 다른 경우에만 붙이면 됨.

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,15 +2,35 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
+/*
+@Entity: JPA가 사용하는 객체임을 명시
+ */
 @Data
+@Entity
 public class Item {
 
+    /*
+    @Id: 해당 프로퍼티가 PK임을 명시
+    @GeneratedValue: auto-increment처럼 DB에서 값을 생성하고 관리하는 PK 전략을 지정
+     */
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /*
+    @Column: 해당 프로퍼티의 이름이 DB 테이블에서의 컬럼명과 다른 경우에 매핑을 위해 사용 (이름이 같다면 생략 가능)
+    - name: 컬럼의 이름 명시. (원래 camel case를 snake case로 자동 변환해주기 때문에 현재의 경우에도 생략 가능함)
+    - length: 컬럼의 길이 명시. 단, 문자열 타입의 컬럼에만 적용 가능 (ex. varchar 10)
+     */
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
 
+    /*
+    JPA 사용 시, public 또는 protected 기본 생성자 필수
+     */
     public Item() {
     }
 
